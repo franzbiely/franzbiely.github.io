17868985e371c2b0db5c9529a1a2ba65c9f9e98e
matt-francis051513 <mrthemetribe@gmail.com>
2019-03-19 21:09:55 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HomeComponent }   from './home/home.component';
+import { ContactComponent }   from './contact/contact.component';
+import { ProjectsComponent }   from './projects/projects.component';
+import { TeamComponent }   from './team/team.component';
+const routes: Routes = [
+  { 
+    path: '',
+    component: DashboardComponent 
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ParticlesModule } from 'angular-particle';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TeamComponent } from './team/team.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    ContactComponent,
+    ProjectsComponent,
+    TeamComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ParticlesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
